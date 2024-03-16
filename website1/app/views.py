55843fecb00575bb6ca73b0e6abb4ed8edab1661
Richie James <richiejames955@gmail.com>
2024-03-16 12:36:09 +0530

    bro's contribution

--- website1/app/views.py
+++ website1/app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render,HttpResponse
 
+
 # Create your views here.
 def home(request):
     return render(request,"index.html")
 
 def handle(request):
     if request.method=="POST":
-        
+        fname=request.POST.get("fname")
+        lname=request.POST.get("lname")
+        email=request.POST.get("email")
+        print(fname,lname,email)
         return HttpResponse("yo man its working")
+    
     return render(request,"signup.html")
 
 
